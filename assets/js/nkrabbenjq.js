049dd1e8c24f3b002670cdaf7b674f7dd9a4dc0f
Nick Krabbenhoeft <n@nkrabben.com>
2015-04-05 22:38:34 -0400

    create text swap jquery script

---
+++ assets/js/nkrabbenjq.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ 
+	$('a.accordion-toggle').click(function() {
+		var el = $(this);
+  		if (el.text() == el.data("text-swap")) {
+    		el.text(el.data("text-original"));
+  		} else {
+    		el.data("text-original", el.text());
+    		el.text(el.data("text-swap"));
+  		}
+	});
+});
